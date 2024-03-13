cda8da856a14ed6fe4c25560671c2eee98dac821
Richard Connor <richard.c.connor@gmail.com>
2024-03-13 11:02:17 +0000

    smoking girl

---
+++ gwt-2.11.0/WebImageTests/src/uk/ac/standrews/cs/client/ImagePanel.java
@@ -0,0 +1,8 @@
+package uk.ac.standrews.cs.client;
+
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Widget;
+
+public class ImagePanel extends Widget {
+    Image i = new Image("https://similarity.cs.st-andrews.ac.uk/mirflickr/images/42/429988.jpg");
+}
