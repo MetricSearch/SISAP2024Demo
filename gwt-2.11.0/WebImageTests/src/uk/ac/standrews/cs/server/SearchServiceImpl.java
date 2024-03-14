71c5f4ef4601fc2798a8e9a8ef27243f69d1a586
Richard Connor <richard.c.connor@gmail.com>
2024-03-14 14:50:31 +0000

    change to .gitignore

--- gwt-2.11.0/WebImageTests/src/uk/ac/standrews/cs/server/SearchServiceImpl.java
+++ gwt-2.11.0/WebImageTests/src/uk/ac/standrews/cs/server/SearchServiceImpl.java
@@ -16,16 +16,19 @@ public class SearchServiceImpl extends RemoteServiceServlet implements
     public String searchServer(String name) {
         try {
             HnswIndex<Integer, float[], Item<Integer, float[]>, Float> index = getIndex(DistanceFunctions.FLOAT_COSINE_DISTANCE);
+            List<Item<Integer, float[]>> queries = Dino2_data.getQueries(1);
             List<Item<Integer, float[]>> data = Dino2_data.getData(1);
+
 //            for( int i = 0; i < 10; i++){
 //                index.add(data.get(i));
 //            }
             index.addAll(data);
             int s = index.size();
+            List<SearchResult<Item<Integer, float[]>, Float>> res = index.findNearest(queries.get(0).vector(), 10);
 
-            return "hello from server after accessing JerkMerk with size " + s;
+            return "hello fffrom server after accessing JerkMerk with " + res.size() + " results.";
         } catch (Exception e) {
-            return "exception: " + e;
+            return "gone wwrong: " + e ;
         }
     }
 
@@ -42,7 +45,6 @@ public class SearchServiceImpl extends RemoteServiceServlet implements
                 .withM(5)
                 .withEfConstruction(100)
                 .withEf(100)
-                .withRemoveEnabled()
                 .build();
 
         return index;
