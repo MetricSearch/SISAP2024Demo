bd8224860a8f99df5aba96e53903f057f2906df9
pchire <ben.claydon2000@gmail.com>
2024-06-26 15:48:51 +0100

    Deleted unused files

--- gwt-2.11.0/WebImageTests/src/uk/ac/standrews/cs/client/ImagePanel.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.standrews.cs.client;
-
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Widget;
-
-public class ImagePanel extends Widget {
-    Image i = new Image("https://similarity.cs.st-andrews.ac.uk/mirflickr/images/42/429988.jpg");
-}
